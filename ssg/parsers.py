827b64ddde5676282c269fbbee6843a46506758c
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-14 20:18:13 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -47,7 +47,6 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path, dest, html)
         filtered = hooks.filter("generate_menu", html, self.base_ext)
         self.write(path, dest, filtered)
         sys.stdout.write(
@@ -61,7 +60,8 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(path, dest, html["html_body"])
+        filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
