2f6c81f1db664374753f548fa4ad3a4dc7ee5e9f
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-14 18:09:58 +0530

    extension

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -8,4 +8,9 @@ def load_module(directory, name):
     sys.path.pop(0)
 
 def load_directory(directory):
-    
+    for path in directory.rglob(".py"):
+        load_module(directory.as_posix(), path.stem)
+
+def load_bundled():
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
