3c0d864b097cf8a391b153727613a3269ca0285f
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-14 20:16:35 +0530

    menu

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -8,6 +8,8 @@ from docutils.core import publish_parts
 from markdown import markdown
 from ssg.content import Content
 
+from ssg import hooks
+
 
 class Parser:
     base_ext = ".html"
@@ -46,6 +48,8 @@ class MarkdownParser(Parser):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
+        filtered = hooks.filter("generate_menu", html, self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
--- ssg/site.py
+++ ssg/site.py
@@ -2,6 +2,8 @@ import sys
 from pathlib import Path
 from ssg import extensions
 
+from ssg import extensions, hooks
+
 
 class Site:
     def __init__(self, source, dest, parsers=None):
@@ -29,6 +31,7 @@ class Site:
 
     def build(self):
         extensions.load_bundled()
+        hooks.event("collect_files", self.source, self.parsers)
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
